2dd1eef492df76814c3afda24c5676268787b12e
Steve Shogren <steve.a.shogren@gmail.com>
2014-03-08 10:54:25 -0500

    Adding a database url

--- src/minty/models/shout.clj
+++ src/minty/models/shout.clj
@@ -2,6 +2,7 @@
   (:require [clojure.java.jdbc :as sql]))
 
 (def spec (or (System/getenv "DATABASE_URL")
+              (System/getenv "HEROKU_POSTGRESQL_AMBER_URL")
               "postgresql://localhost:5432/shouter"))
 
 (defn all []
