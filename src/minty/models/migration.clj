e80892071fd428604bc436c85d551f620c8533be
Steve Shogren <steve.a.shogren@gmail.com>
2014-03-11 17:07:54 -0400

    Getting the tables to drop and recreate

--- src/minty/models/migration.clj
+++ src/minty/models/migration.clj
@@ -5,17 +5,25 @@
             (System/getenv "HEROKU_POSTGRESQL_AMBER_URL")
             "postgresql://localhost:5432/shouter"))
 
+(def table-list ["payments", "buckets"])
+
+(defn drop-all []
+  (sql/execute! db [(sql/drop-table-ddl "buckets")])
+  (sql/execute! db [(sql/drop-table-ddl "payments")]))
+
+(defn commify [col]
+  (->> col
+   (map #(str "'" % "'"))
+   (reduce #(str %1 ", " %2))))
+
 (defn migrated? []
-  (sql/db-do-commands db
-                      (sql/execute! (sql/drop-table-ddl "buckets"))
-                      (sql/execute! (sql/drop-table-ddl "payments")))
-  false
-  #_(-> (sql/query db
-                   [(str "select count(*) from information_schema.tables "
-                         "where table_name='payments'")])
-        first :count pos?))
+  (-> (sql/query db
+                 [(str "select count(*) from information_schema.tables "
+                       "where table_name in (" (commify table-list) ")")])
+      first :count (= (count table-list))))
 
 (defn migrate []
+  (drop-all)
   (when (not (migrated?))
     (print "Creating database structure...") (flush)
     (sql/db-do-commands db
