d072ac5cce5a8539f9a1ee7e83bfe9e3514bf4fd
Steve Shogren <steve.a.shogren@gmail.com>
2014-03-12 07:19:36 -0400

    Getting the tables to drop/create on every app start

--- src/minty/models/migration.clj
+++ src/minty/models/migration.clj
@@ -5,39 +5,41 @@
             (System/getenv "HEROKU_POSTGRESQL_AMBER_URL")
             "postgresql://localhost:5432/shouter"))
 
-(def table-list ["payments", "buckets"])
-
-(defn drop-all []
-  (sql/execute! db [(sql/drop-table-ddl "buckets")])
-  (sql/execute! db [(sql/drop-table-ddl "payments")]))
+(def table-list ["payments" "buckets"])
 
 (defn commify [col]
   (->> col
    (map #(str "'" % "'"))
    (reduce #(str %1 ", " %2))))
 
-(defn migrated? []
+(defn tables-exist? []
   (-> (sql/query db
                  [(str "select count(*) from information_schema.tables "
                        "where table_name in (" (commify table-list) ")")])
       first :count (= (count table-list))))
 
+(defn drop-all []
+  (print "Dropping all tables") (flush)
+  (if (tables-exist?)
+    (sql/db-do-commands db
+                        (sql/drop-table-ddl :buckets)
+                        (sql/drop-table-ddl :payments))))
+
 (defn migrate []
   (drop-all)
-  (when (not (migrated?))
-    (print "Creating database structure...") (flush)
-    (sql/db-do-commands db
-                        (sql/create-table-ddl
-                         :buckets
-                         [:id :serial "PRIMARY KEY"]
-                         [:name :varchar "NOT NULL"]
-                         [:created_at :timestamp
-                          "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"])
-                        (sql/create-table-ddl
-                         :payments
-                         [:id :serial "PRIMARY KEY"]
-                         [:paid_to :varchar "NOT NULL"]
-                         [:amount :decimal "NOT NULL"]
-                         [:created_at :timestamp
-                          "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]))
-    (println " done")))
+  (print "Creating database structure...") (flush)
+  (sql/db-do-commands db
+                      (sql/create-table-ddl :buckets
+                                            [:id :serial "PRIMARY KEY"]
+                                            [:name :varchar "NOT NULL"]
+                                            [:created_at :timestamp
+                                             "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"])
+                      (sql/create-table-ddl :payments
+                                            [:id :serial "PRIMARY KEY"]
+                                            [:paid_to :varchar "NOT NULL"]
+                                            [:amount :decimal "NOT NULL"]
+                                            [:created_at :timestamp
+                                             "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]))
+  (println " done"))
+
+(migrate)
